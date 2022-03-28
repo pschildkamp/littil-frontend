763df91f2206fa9adce629fae542d998463557f6
Anja van Hagen <anja.vanhagen@ns.nl>
2022-03-28 21:13:42 +0200

    Add tailwindcss and remove material

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    fontFamily: {
+      body: ["Poppins"],
+    },
+    colors: {
+      white: "#ffffff",
+      black: "#000000",
+      gray: "#acacac",
+      yellow: {
+        50: "#efa55c",
+        100: "#f18f2d",
+        200: "#e57200",
+      },
+      blue: {
+        100: "#a1c5ea",
+        200: "#2d2e83",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
