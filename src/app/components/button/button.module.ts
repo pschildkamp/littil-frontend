88dfba75b97c72c28851b5d9304ffd739cf291e7
Anja van Hagen <anja.vanhagen@ns.nl>
2022-03-28 21:16:10 +0200

    Add buttonModule

---
+++ src/app/components/button/button.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ButtonComponent } from './button.component';
+
+@NgModule({
+  declarations: [ButtonComponent],
+  imports: [],
+  providers: [],
+  exports: [ButtonComponent],
+})
+export class ButtonModule {}
