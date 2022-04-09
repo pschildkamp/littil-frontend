bfcf635d1fb4e33c9b57bf84bc08110361b75dec
Anja van Hagen <anja.vanhagen@ns.nl>
2022-04-09 17:50:25 +0200

    Add component FormInputSelectComponent

---
+++ src/app/components/forms/select-input/form-input-select.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormInputSelectComponent } from './form-input-select.component';
+
+@NgModule({
+  declarations: [FormInputSelectComponent],
+  imports: [CommonModule],
+  providers: [],
+  exports: [FormInputSelectComponent],
+})
+export class FormInputSelectModule {}
