bf235838d40e93ad663dbbef9538542bf8446350
Tubby <git@tubby.nl>
2022-04-11 21:35:13 +0200

    flattened teacher model to single model with optional id

--- src/app/model/teacher.ts
+++ src/app/model/teacher.ts
@@ -1,13 +1,9 @@
 export class Teacher {
-  static CREATED_EVENT = 'teacher-created';
-  static UPDATED_EVENT = 'teacher-updated';
-  static DELETED_EVENT = 'teacher-deleted';
-
-  id: string
-  firstName: string;
-  surname: string;
-  email: string;
-  postalCode: string;
+  id: string | undefined;
+  firstName: string | undefined;
+  surname: string | undefined;
+  email: string | undefined;
+  postalCode: string | undefined;
   country: string = "nl"
 
   constructor(id: string, firstName: string, surname: string, email: string, postalCode: string) {
@@ -17,26 +13,4 @@ export class Teacher {
     this.email = email;
     this.postalCode = postalCode;
   }
-}
-
-export class TeacherUpdate {
-  id: string;
-  firstName: string | undefined;
-  surname: string | undefined;
-  email: string | undefined;
-  postalCode: string | undefined;
-  country: string = "nl"
-
-  constructor(id: string) {
-    this.id = id;
-  }
-}
-
-export class TeacherCreate {
-  firstName: string | undefined;
-  surname: string | undefined;
-  email: string | undefined;
-  password: string | undefined;
-  postalCode: string | undefined;
-  country: string = "nl"
 }
