a16948f9c8f85a9527f36845943ab7427c926f5c
Tubby <git@tubby.nl>
2022-06-08 14:03:55 +0200

    rename to ApiService

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,7 +5,7 @@ import {Observable} from "rxjs";
 @Injectable({
   providedIn: 'root'
 })
-export class ApiServiceService {
+export class ApiService {
 
   constructor(
     private schoolService: SchoolService,
